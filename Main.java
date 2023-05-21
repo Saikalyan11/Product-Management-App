77b096b3f345b0789d1d7b050ea8d22ff5a3f12a
Sai Kalyan <kalyan9700128@gmail.com>
2023-05-22 01:06:20 +0530

    list

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,9 @@ public class Main {
         service.addProducts(new Products("Mobile", "REALME", "USA", 3));
         service.addProducts(new Products("Pen", "OX", "USA", 1));
 
-        // List<Products> products = service.getAllProducts();
-        // for (Products p : products)
-        // System.out.println(p);
+        List<Products> products = service.getAllProducts();
+        for (Products p : products)
+            System.out.println(p);
 
         Products p = service.getProduct("laptop");
         System.out.println(p);
